7693f2577975c979fd28c10de825c5a755311d53
Ethan-Coder3000 <95977294+Ethan-Coder3000@users.noreply.github.com>
2023-08-10 16:05:14 +0200

    cleanup

---
+++ tests/models/test_responseModel.py
@@ -0,0 +1,26 @@
+import unittest
+from pydantic import ValidationError
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from models.responseModel import MathProblemResponse
+
+class TestMathProblemResponse(unittest.TestCase):
+
+    def test_valid_math_problem_response(self):
+        problem = "2 + 2"
+        solution = "4"
+        response = MathProblemResponse(problem=problem, solution=solution)
+        self.assertEqual(response.problem, problem)
+        self.assertEqual(response.solution, solution)
+
+    def test_missing_fields(self):
+        with self.assertRaises(ValidationError):
+            MathProblemResponse()
+
+    def test_invalid_types(self):
+        with self.assertRaises(ValidationError):
+            MathProblemResponse(problem=42, solution="four")
+
+if __name__ == '__main__':
+    unittest.main()
