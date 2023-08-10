81ec97495f80ff4d0f1cb1fb8dcb9686f34ffb7d
Ethan-Coder3000 <95977294+Ethan-Coder3000@users.noreply.github.com>
2023-08-10 16:29:56 +0200

    cleanup

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,5 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from main import app
 
 client = TestClient(app)
